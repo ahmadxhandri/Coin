1cf2afa35824ec7f02fc5e25f7b8234eefeb9b8c
ahmadxhandri <ahmadxhandri@gmail.com>
2026-01-30 23:25:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -968,7 +968,7 @@ async def websocket_endpoint(ws: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         app,
         host="0.0.0.0",
